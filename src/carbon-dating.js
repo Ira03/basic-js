4f83695afe3090f22570718a8b97b6f35cb1edd7
Iryna Yakubenia <i.yakubenia@gmail.com>
2020-10-03 23:53:49 +0300

    carbon-dating 100%

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,11 +4,21 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  if (typeof sampleActivity === 'string' && typeof +sampleActivity === 'number') {
+
+  if (sampleActivity == undefined && typeof sampleActivity !== 'string' && !isFinite(sampleActivity) && sampleActivity <= 0 && !(+sampleActivity < MODERN_ACTIVITY)) {
+    return false;
+  } 
     let decayRate =  0.693 / HALF_LIFE_PERIOD;
     let sampleAge = Math.ceil(Math.log(MODERN_ACTIVITY / +sampleActivity) / decayRate);
-    return sampleAge;
-  } else {
-    return false;
-  }
+    if(isFinite(sampleAge) && !isNaN(sampleAge) && !sampleAge < 0) {
+      return sampleAge;
+    } else {
+      return false;
+    }
+    
+    
+
+ 
+  
+  
 };
